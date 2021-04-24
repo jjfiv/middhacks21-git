9a35756dd54751e2b6a69b3c81ec02376a3a6853
Jason Rickenbacher <56455108+JRickenbacher@users.noreply.github.com>
2021-04-24 11:51:48 -0400

    . -> *

--- jasonr.py
+++ jasonr.py
@@ -4,10 +4,10 @@ def hello():
 def print_triangle(n, rev=False):
     if rev:
         for i in reversed(range(n)):
-            print("." * i)
+            print("*" * i)
     else:
         for i in range(n):
-            print("." * i)
+            print("*" * i)
 
 
 # Yes, the example file is in Python.
