46eede985d0444d7a5bd3f29999b264337a00bb7
John Foley <johnf@middlebury.edu>
2021-04-24 13:25:16 +0000

    upgrade starter py

--- johnf.py
+++ johnf.py
@@ -1,3 +1,18 @@
+
+def hello():
+    return "Hello World!";
+
+def print_triangle(n, rev=False):
+    if rev:
+        for i in reversed(range(n)):
+            print("." * i)
+    else:
+        for i in range(n):
+            print("." * i)
+
+
 # Yes, the example file is in Python.
 if __name__ == '__main__':
-  print("Hello World!")
+  print_triangle(10)
+  print(hello())
+  print_triangle(10, rev=True)
